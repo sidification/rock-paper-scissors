5d8224d4e74d26505721ad952f0a9a93a514af9a
Sid Ahuja <ahuja12@illinois.edu>
2023-05-06 11:23:44 -0600

    updated external.js

--- external.js
+++ external.js
@@ -2,22 +2,48 @@ const items = ["rock", "paper", "scissors"]
 let cChoice
 let pChoice
 
-function getComputerChoice(items) {
+function getComputerChoice() {
   cChoice = items[Math.floor(Math.random() * items.length)]
-  console.log(cChoice)
-  // return choice
+  console.log(`computer chose ${cChoice}`)
+  return cChoice
 }
 
-function playerChoice(items) {
+function getPlayerChoice() {
   pChoice = prompt("what do you choose? type 'r', 'p', 's' in the box")
   if (pChoice == "r") {
     console.log("you chose rock")
+    return "rock"
   } else if (pChoice == "p") {
     console.log("you chose paper")
+    return "paper"
   } else {
     console.log("you chose scissors")
+    return "scissors"
   }
-  return pChoice
 }
 
-// playerChoice(items)
+function roundWinner(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+    return 1
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    return 0
+  }
+}
+
+function game() {
+  let win = 0
+  for (let i = 0; i < 5; i++) {
+    console.log(`this is round ${i}`)
+    let out = roundWinner(getPlayerChoice(), getComputerChoice())
+    win = win + out
+  }
+  win > 2 ? console.log("You win the game!") : console.log("You lose the game ")
+}
+
+game()
